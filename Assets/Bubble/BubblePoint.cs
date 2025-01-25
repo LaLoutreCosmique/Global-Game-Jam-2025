fe18e7d24a5a54a98de6d1d240437147522f4437
La Loutre Cosmique <sushi.laque77@gmail.com>
2025-01-25 02:18:34 +0100

    feat: repulsor

--- Assets/Bubble/BubblePoint.cs
+++ Assets/Bubble/BubblePoint.cs
@@ -1,4 +1,5 @@
 using System;
+using Obstacles;
 using UnityEngine;
 
 namespace Bubble
@@ -7,11 +8,27 @@ namespace Bubble
     {
         [SerializeField] Bubble parent;
 
+        Rigidbody2D m_Rb2D;
+
+        void Awake()
+        {
+            m_Rb2D = GetComponent<Rigidbody2D>();
+        }
+
         void OnCollisionEnter2D(Collision2D other)
         {
             if (!other.transform.CompareTag("Enemy")) return;
             
             parent.Pop();
+        }
+
+        void OnTriggerStay2D(Collider2D other)
+        {
+            if (!other.transform.CompareTag("ForceField")) return;
+
+            ForceField field = other.GetComponent<ForceField>();
+            print(field.transform.up);
+            m_Rb2D.AddForce(field.transform.up * field.Force);
         }
     }
 }
