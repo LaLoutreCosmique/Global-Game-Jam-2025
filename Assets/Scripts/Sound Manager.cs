5b07e105f7b4fd06964a89afc5bf298db8ffb3e0
GeekhaNoth <axel.riou405@gmail.com>
2025-01-25 22:25:48 +0100

    Ajout du sound manager dans le menu

---
+++ Assets/Scripts/Sound Manager.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    [SerializeField] private AudioMixer mixer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeMusicVolume(float value)
+    {
+        mixer.SetFloat("musicVolume", Mathf.Log10(musicSlider.value) * 20);
+    }
+
+    public void ChangeSfxVolume()
+    {
+        mixer.SetFloat("SfxVolume", Mathf.Log10(sfxSlider.value) * 20);
+    }
+}
