b65f2cf3cc6165ce946384462bebb19625891f37
GeekhaNoth <axel.riou405@gmail.com>
2025-01-25 15:15:12 +0100

    WIP settings menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,6 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Canvas menuCanvas;
+    [SerializeField] private Canvas settingCanvas;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,16 +19,25 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
+        Debug.Log("StartGame");
         SceneManager.LoadScene("Game");
     }
 
     public void SettingsGame()
     {
-        //Gerer les settings
+        menuCanvas.gameObject.SetActive(false);
+        settingCanvas.gameObject.SetActive(true);
     }
 
     public void ExitGame()
     {
+        Debug.Log("ExitGame");
         Application.Quit();
+    }
+
+    public void BackToMainMenu()
+    {
+        settingCanvas.gameObject.SetActive(false);
+        menuCanvas.gameObject.SetActive(true);
     }
 }
