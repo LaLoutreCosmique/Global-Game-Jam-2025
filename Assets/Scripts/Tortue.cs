1bbfbe57dbdae1e856e73bbd5967e01e17ddc103
GeekhaNoth <axel.riou405@gmail.com>
2025-01-26 13:49:18 +0100

    Ajout Flip Tortue

--- Assets/Scripts/Tortue.cs
+++ Assets/Scripts/Tortue.cs
@@ -1,17 +1,29 @@
 using System;
+using DG.Tweening;
 using UnityEngine;
 
 public class Tortue : MonoBehaviour
 {
      [SerializeField]private float moveSpeed;
+     private bool direction = true;
+     public float rotationSpeed = 0.5f;
 
      private void Update()
      {
-         transform.position = new Vector3(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+         if (!direction)
+         {
+             transform.position = new Vector3(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+         }
+         else
+         {
+             transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+         }
      }
 
      private void OnTriggerEnter2D(Collider2D other)
     {Debug.Log("collision");
+        transform.DOScaleY(-transform.localScale.y, rotationSpeed);
+        direction = !direction;
         if (other.gameObject.CompareTag("turtlezone"))
         {
             Debug.Log("turtle");
